ec249049113891a164d25d22a307744f4467f33a
Vanson Samuel <vs@binq.com>
2009-12-14 06:26:50 -0500

    version updated

--- lib/winch.rb
+++ lib/winch.rb
@@ -5,7 +5,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Winch
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   
   autoload :Base, 'winch/base'
 end
